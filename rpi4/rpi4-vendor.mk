47fae05fafbb2cbbe79752ea521fcce69e9e30e9
Konsta <konsta09@gmail.com>
2023-07-27 10:20:18 +0300

    rpi4: update bluetooth firmware
    * https://github.com/RPi-Distro/bluez-firmware/tree/9556b08ace2a1735127894642cc8ea6529c04c90/broadcom
    * Fixes connecting BLE devices
    * Don't copy bt firmware as it's built into the kernel for btlinux
    
    Change-Id: Ia6fffef2c85553b728ed250298c08e25b5adde9d

--- rpi4/rpi4-vendor.mk
+++ rpi4/rpi4-vendor.mk
@@ -4,7 +4,3 @@
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/brcm/rpi4
-
-PRODUCT_COPY_FILES += \
-    vendor/brcm/rpi4/proprietary/vendor/firmware/brcm/BCM4345C0.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/brcm/BCM4345C0.hcd \
-    vendor/brcm/rpi4/proprietary/vendor/firmware/brcm/BCM4345C5.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/brcm/BCM4345C5.hcd
